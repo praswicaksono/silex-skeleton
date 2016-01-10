cedf7cf08c3aa8e8a2e9ec0a7b6eb945ccc531d6
Prasetyo <jowy@oiry.net>
2016-01-10 16:09:25 +0700

    add gitignore

--- web/index.php
+++ web/index.php
@@ -4,6 +4,11 @@ use Silex\Application;
 
 require __DIR__ . '/../vendor/autoload.php';
 
+if (! file_exists(__DIR__ . '/../app/config.php')) {
+    throw new Exception("app/config.php not found");
+
+}
+
 $app = new Application(require __DIR__ . '/../app/config.php');
 
 require __DIR__ . '/services.php';
